79dc178de84218d690622e94ab1719cb9129351a
gracekim527 <lemonherb0323@naver.com>
2023-08-04 01:18:58 +0900

    :white_check_mark: [Add] Section 8

---
+++ herb/Pinch/Pinch/Data/PageData.swift
@@ -0,0 +1,14 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by 김은혜 on 2023/08/04.
+//
+
+import Foundation
+
+// 수동데이터 입력
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
