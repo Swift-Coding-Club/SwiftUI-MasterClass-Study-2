e93d9badaf60150f2c241833144c5b75bf7badbb
ltw <witlim1225@gmail.com>
2023-08-13 12:08:37 +0900

    [ADD] Section 7 시작

---
+++ Tony/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Tony Lim on 2023/08/13.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
